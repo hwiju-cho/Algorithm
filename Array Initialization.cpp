09a14efcf0a99c600895e6a6601eac192f94a600
hj.cho <scyho0819@gmail.com>
2021-09-02 01:53:05 +0900

    Initializing Array

--- Array Initialization.cpp
+++ Array Initialization.cpp
@@ -31,4 +31,20 @@ int main()
     {
         fill(arr2[i],arr2[i]+21,0);
     }
+
+    // 4. copy
+    int arr3[5];
+    int arr4[5];
+    fill(arr3,arr3+5,0);
+    
+    copy(arr3,arr3+5,arr4);
+    
+    int arr5[10][10];
+    int arr6[10][10];
+    for (int i=0;i<10;i++)
+    {
+        fill(arr5[i],arr5[i]+10,0);
+    }
+
+    copy(&arr5[0][0],&arr5[0][0]+100, &arr6[0][0]);
 }
